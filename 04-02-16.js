2cc014896596a8455050fbb54cbf153942b96116
Anna Casper <anna.casper3@gmail.com>
2016-04-02 09:43:06 -0600

    initial commit

---
+++ 04-02-16.js
@@ -0,0 +1,19 @@
+// The Hamming weight of a string is the number of symbols that are different from the zero-symbol of the alphabet used.
+// There are several algorithms for efficient computing of the Hamming weight for numbers.
+// In this Kata, speaking technically, you have to find out the number of '1' bits in a binary representation of a number.
+// Thus,
+
+//hammingWeight(10) // 1010  =&gt; 2
+//hammingWeight(21) // 10101 =&gt; 3
+
+//The interesting part of this task is that you have to do it without string operation
+
+//using toString()
+hammingWeight = function (num) {
+  parseInt(num, 10).toString(2);
+  console.log(num);
+  return num;
+}
+
+hammingWeight(10);
+hammingWeight(21);
